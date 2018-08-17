c50ff3e901193431d6acb8e75f2a373110ecee72
unknown <brunosouzasantos07@gmail.com>
2018-08-17 00:07:21 -0300

    #Initial Commit - Add model to information of clients computers

---
+++ Model/ComputerDetails.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonitorSystem.Core.Model
+{
+    public class ComputerDetails
+    {
+
+        #region Public Members
+
+        public string NameComputer { get; set; }
+        public string Ipv4 { get; set; }
+        public string UserName { get; set; }
+        public object Processes { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        public ComputerDetails() { }
+
+        /// <summary>
+        /// Full constructor
+        /// </summary>
+        /// <param name="nameComputer"></param>
+        /// <param name="ipv4"></param>
+        /// <param name="userName"></param>
+        /// <param name="processes"></param>
+        public ComputerDetails(string nameComputer, string ipv4, string userName, object processes)
+        {
+            NameComputer = nameComputer;
+            Ipv4 = ipv4;
+            UserName = userName;
+            Processes = processes;
+        }
+
+        #endregion
+
+        #region Equals and Hashcode
+
+        public override bool Equals(object obj)
+        {
+            var details = obj as ComputerDetails;
+            return details != null &&
+                   NameComputer == details.NameComputer &&
+                   Ipv4 == details.Ipv4 &&
+                   UserName == details.UserName &&
+                   EqualityComparer<object>.Default.Equals(Processes, details.Processes);
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 228897068;
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(NameComputer);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Ipv4);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(UserName);
+            hashCode = hashCode * -1521134295 + EqualityComparer<object>.Default.GetHashCode(Processes);
+            return hashCode;
+        }
+
+
+        #endregion
+
+    }
+}
